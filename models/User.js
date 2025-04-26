79331f5f2bfc634157a8e290459c2a04f9ccf008
manuelodiis <manuelodiis@gmail.com>
2025-04-26 01:42:28 -0700

    fix

--- models/User.js
+++ models/User.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    _id: { type: string, required: true },
-    name: { type: string, required: true },
-    email: { type: string, required: true, unique: true },
-    imageUrl: { type: string, required: true },
-    cartItem: { type: object, default: {} },
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    imageUrl: { type: String, required: true },
+    cartItems: { type: Object, default: {} },
   },
   { minimize: false }
 );
